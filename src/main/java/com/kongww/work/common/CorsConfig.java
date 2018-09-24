06a16051ef6d06fa5fef4fa7d5b08267982d8b2d
qiuguanlin <578482558@qq.com>
2018-09-24 12:15:29 +0800

    跨域问题

---
+++ src/main/java/com/kongww/work/common/CorsConfig.java
@@ -0,0 +1,33 @@
+package com.kongww.work.common;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @Author: QiuGuanLin
+ * @Description:
+ * @Date: 12:02 2018/9/24
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        // 设置你要允许的网站域名，如果全允许则设为 *
+        config.addAllowedOrigin("*");
+        // 如果要限制 HEADER 或 METHOD 请自行更改
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        // 这个顺序很重要哦，为避免麻烦请设置在最前
+        bean.setOrder(0);
+        return bean;
+    }
+}
